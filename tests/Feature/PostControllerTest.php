2b1ce3fe99362d75895f657de3e6e83103370025
Md Harunur Rashid <harunur@ba-systems.com>
2024-04-22 18:11:59 +0600

    test case maintain

--- tests/Feature/PostControllerTest.php
+++ tests/Feature/PostControllerTest.php
@@ -25,4 +25,94 @@ class PostControllerTest extends TestCase
             ->assertJsonCount(10, 'data');
     }
 
+    public function test_it_can_create_new_post(): void
+    {
+        $post = Post::factory()->make();
+
+        $response = 
+            $this->postJson(route('posts.store'), [
+                'title' => $post->title,
+                'author' => $post->author,
+                'body' => $post->body,
+            ]);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'data' => [
+                    'title' => $post->title,
+                    'author' => $post->author,
+                    'body' => $post->body,
+                ]
+            ]);
+        
+        $this->assertDatabaseHas('posts', [
+            'title' => $post->title,
+            'author' => $post->author,
+            'body' => $post->body,
+        ]);
+    }
+
+    public function test_title_is_required(): void
+    {
+        $post = Post::factory()->create([
+            'author' => fake()->name,
+            'body' => fake()->paragraph,
+        ]);
+
+        $response = $this->postJson(route('posts.store'), [
+            'author' => $post->author,
+            'body' => $post->body,
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_it_can_return_one_post(): void
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->getJson(route('posts.show', $post));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_it_can_update_a_post(): void
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->putJson(route('posts.update', $post), [
+            'title' => 'New title',
+            'author' => 'New author',
+            'body' => 'New body',
+        ]);
+
+        $post->refresh();
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'title' => 'New title',
+                    'author' => 'New author',
+                    'body' => 'New body',
+                ]
+            ]);
+
+        $this->assertEquals('New title', $post->title);
+        $this->assertEquals('New author', $post->author);
+        $this->assertEquals('New body', $post->body);
+    }
+
+
+    public function test_it_can_delete_post(): void
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->deleteJson(route('posts.destroy', $post));
+
+        $response->assertStatus(204);
+        
+        $this->assertDatabaseMissing('posts', ['id' => $post->id]);
+    }
+
 }
