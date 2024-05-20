cd22bf4e4bcd455c2ca10d1ff72e7613795ff4ee
ahmharunurrashid <ahmharunurrashid@gmail.com>
2024-05-20 22:58:30 +0600

    some new code added

---
+++ resources/js/Pages/About.jsx
@@ -0,0 +1,12 @@
+import Guest from "@/Layouts/GuestLayout";
+
+export default function About() {
+    return (
+        <div>
+            <Guest>
+                <h1>About Page</h1>
+            </Guest>
+
+        </div>
+    );
+}
---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="my-5 text-xl font-semibold text-center">
+                Welcome to My Blog
+            </h1>
+
+        </div>
+    );
+}
